c5cd166286c1043b3ca5194baddebb8f96669d0b
Daniel52x <danielcost0705@gmail.com>
2020-06-25 15:53:04 -0300

    alteracoes

---
+++ challenge-20/aula/js.js
@@ -0,0 +1,12 @@
+(function( win, doc ){
+    'use strict';
+    // console.log( win === window);
+    // if(win === window)
+    //     console.log( ' win é igual a window ' );
+
+    // win.alert( 'Mensagem' );
+    // window.prompt( 'Mensagem?' );
+    console.log( doc.getElementsByClassName( 'my-link' ) );
+
+
+})(window, document);
